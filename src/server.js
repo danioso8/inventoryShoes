fd372e4bff8124104ae22c27ebf5174bc0ef11d3
danioso8 <danioso8@hotmail.com>
2025-11-25 00:11:24 -0500

    Fix CORS to allow multiple localhost ports

--- src/server.js
+++ src/server.js
@@ -11,9 +11,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'], // URL del frontend React
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
